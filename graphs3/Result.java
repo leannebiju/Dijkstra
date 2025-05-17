aedb171258171a5c4b44bd88d443596b2d2ce2b2
Leanne Biju <148006830+leannebiju@users.noreply.github.com>
2025-05-17 11:30:31 +0530

    Create Result.java

---
+++ graphs3/Result.java
@@ -0,0 +1,12 @@
+package graphs3;
+public class Result 
+{
+    int[] distances;
+    int[] predecessors;
+
+    public Result(int[] distances, int[] predecessors) 
+    {
+        this.distances = distances;
+        this.predecessors = predecessors;
+    }
+}
